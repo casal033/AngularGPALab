06df2c65c7f98fc11f1b1bffdda9b0d29f167c72
casal033 <casal033@morris.umn.edu>
2014-09-16 11:06:12 -0500

    Made a new html for the GPA App. Added links and a dropdown to the page

---
+++ public/javascripts/gpa.js
@@ -0,0 +1,26 @@
+/**
+ * Created by casal033 on 9/16/14.
+ */
+angular.module('gpaApp', ['ui.bootstrap']);
+
+function DropdownCtrl($scope) {
+    $scope.items = [
+        'The first choice!',
+        'And another choice for you.',
+        'but wait! A third!'
+    ];
+
+    $scope.status = {
+        isopen: false
+    };
+
+    $scope.toggled = function(open) {
+        console.log('Dropdown is now: ', open);
+    };
+
+    $scope.toggleDropdown = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+        $scope.status.isopen = !$scope.status.isopen;
+    };
+}
